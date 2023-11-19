95ff3a67f6a6afb9a70d9cb864b317db62cef277
Gary Woodfine <gary.woodfine@threenine.co.uk>
2023-11-19 18:21:12 +0000

    adding tailwind

---
+++ src/front/tailwind.config.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    mode: 'jit',
+    content: [
+        './wwwroot/**/*.html',
+        './**/*.razor',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
