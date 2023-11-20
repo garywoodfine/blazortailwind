531a6a4b030dd87e1abe883ae2bb9372f279ba59
Gary Woodfine <lnb0l9dc@duck.com>
2023-11-20 21:15:49 +0000

    Removed .idea

---
+++ src/front/tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+    darkMode: 'media',
+    content: [
+        './**/*.{razor,html,cshtml}',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/forms'),
+        require('@headlessui/tailwindcss'),
+    ],
+};
