63b9001180aef75f7eeeb322b3f005e0026ded16
Gary Woodfine <lnb0l9dc@duck.com>
2023-11-20 11:27:17 +0000

    figured out how to add TypeScript Tailwind

---
+++ src/front/tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  mode: 'jit',
+  darkMode: 'media',
+  content: [
+    './**/*.{razor,html,cshtml}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@headlessui/tailwindcss'),
+  ],
+} satisfies Config
+
